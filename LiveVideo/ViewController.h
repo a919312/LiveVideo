fbcdbc892649aa9d8f8b69d0995cb3629615bc9e
songwentong <songwentong@mobimagic.com>
2016-03-02 11:43:23 +0800

    创建项目

---
+++ LiveVideo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LiveVideo
+//
+//  Created by SongWentong on 3/2/16.
+//  Copyright © 2016 SongWentong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LiveVideo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LiveVideo
+//
+//  Created by SongWentong on 3/2/16.
+//  Copyright © 2016 SongWentong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
